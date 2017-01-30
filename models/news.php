4b56fab588bc261ffaf4e42c7c51df3afe66ac78
MaxVell89 <max_vell_89@mail.ru>
2017-01-30 13:14:33 +0300

    Abstract model

--- models/news.php
+++ models/news.php
@@ -1,21 +1,13 @@
 <?php 
 
-require __DIR__ . '/../classes/db.php';
+class News
+	extends AbstractModel {
+		public $id;
+		public $title;
+		public $text;
 
-class News {
-	public $id;
-	public $title;
-	public $text;
-
-	public static function getAll() {
-		$db = new DB;
-		return $db->queryAll('SELECT * FROM articles', 'News');
-	}
-
-	public static function getOne($id) {
-		$db = new DB;
-		return $db->queryOne('SELECT * FROM articles WHERE id = ' . $id, 'News');
-	}
+		protected static $table = 'articles';
+		protected static $class = 'News';
 }
 
  ?>
